b815fe758c820069d6f83bb7acbb855ccb2e0e75
josh <joshuajames355@gmail.com>
2022-01-19 20:41:14 +0000

    new asset loading system

---
+++ src/assets.ts
@@ -0,0 +1,23 @@
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { TextureLoader, Mesh, Texture } from "three";
+
+//loads all assets and writes them to global vars
+
+const gltf_loader = new GLTFLoader();
+const tex_loader = new TextureLoader();
+
+export var road_model: Mesh;
+export var road_diffuse: Texture;
+export var road_normal: Texture;
+
+gltf_loader.load("/art/road/road.glb", function (gltf) {
+    road_model = gltf.scene.children[0] as Mesh;
+});
+tex_loader.load("/art/road/diffuse.png", function (tex) {
+    tex.flipY = false;
+    road_diffuse = tex;
+});
+tex_loader.load("/art/road/normal.png", function (normal_teture) {
+    normal_teture.flipY = false;
+    road_normal = normal_teture;
+});
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-
-import { Scene, PerspectiveCamera, WebGLRenderer, Vector3, PointLight, AmbientLight, DirectionalLight } from 'three';
-import { generate_terrain } from "./generator"
-import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls';
+import { Scene, PerspectiveCamera, WebGLRenderer, AmbientLight, DirectionalLight, DefaultLoadingManager } from "three";
+import { FirstPersonControls } from "three/examples/jsm/controls/FirstPersonControls";
+import generate_city from "./city_gen";
+import "./assets";
 
 const scene = new Scene();
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -9,9 +9,8 @@ const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight,
 const light = new DirectionalLight(0xffffff);
 scene.add(light);
 
-const light2 = new AmbientLight(0xFFFFFF, 1);
+const light2 = new AmbientLight(0xffffff, 0.2);
 scene.add(light2);
-
 
 const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -21,12 +20,19 @@ const controls = new FirstPersonControls(camera, renderer.domElement);
 controls.movementSpeed = 0.05;
 controls.lookSpeed = 0.0005;
 
-scene.add(generate_terrain(256, 256, new Vector3(-100, -50, -100)));
 camera.position.z = 1;
 
 var frame_times: number[] = [];
 var last_time = performance.now();
 const NUM_FRAMES_ROLLING_AVERAGE = 5;
+
+DefaultLoadingManager.onLoad = function () {
+    console.log("Loading complete!");
+    generate_city(scene);
+};
+DefaultLoadingManager.onProgress = function (url, itemsLoaded, itemsTotal) {
+    console.log("Loading file: " + url + ".\nLoaded " + itemsLoaded + " of " + itemsTotal + " files.");
+};
 
 function animate() {
     requestAnimationFrame(animate);
@@ -37,7 +43,7 @@ function animate() {
 
     controls.update(delta_time);
 
-    if (frame_times.push(delta_time) > NUM_FRAMES_ROLLING_AVERAGE) frame_times.shift()
+    if (frame_times.push(delta_time) > NUM_FRAMES_ROLLING_AVERAGE) frame_times.shift();
     var fps = (1000 * frame_times.length) / frame_times.reduce((a, b) => a + b);
 
     last_time = current_time;
